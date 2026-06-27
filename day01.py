22a80609e79fe3a5aaeed7110f3f7de8d121986b
Uday Badhai <ukanhabadhaiuday@gmail.com>
2026-06-27 21:34:56 +0530

    Add user input for name and age with voting eligibility

---
+++ day01.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Name:", name)
+print("Age:", age)
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
